3ca5237345cf53d8975db58a04d61ecb45b784bc
Advait "Conty" Contractor <advaitthepro@outlook.com>
2023-06-10 15:28:46 +0800

    Initial Commit

---
+++ blogapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blogapp
+//
+//  Created by Milind Contractor on 10/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blogapp/blogappApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogappApp.swift
+//  blogapp
+//
+//  Created by Milind Contractor on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct blogappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
